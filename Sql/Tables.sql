a48cb35e3633fea88ae4608d464dbaa3ef49bfa8
akhileshreddyyy <akhilesh19230@iiitd.ac.in>
2021-04-03 11:57:15 +0530

    Minor changes

--- Sql/Tables.sql
+++ Sql/Tables.sql
@@ -1,7 +1,6 @@
 DROP DATABASE IF EXISTS `GraduateGenie`;
 CREATE DATABASE `GraduateGenie` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE GraduateGenie;
-
 DROP TABLE IF EXISTS `Administration`;
 CREATE TABLE `Administration` (
   `Admin ID` varchar(10) NOT NULL,
@@ -45,6 +44,30 @@ CREATE TABLE `Company` (
   UNIQUE KEY `Company ID_UNIQUE` (`Company ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+DROP TABLE IF EXISTS `Student`;
+CREATE TABLE `Student` (
+  `Student ID` varchar(10) NOT NULL,
+  `First_Name` varchar(100) NOT NULL,
+  `Last_Name` varchar(100) NOT NULL,
+  `College ID` varchar(10) NOT NULL,
+  `House Number` varchar(20) NOT NULL,
+  `Building,Street,Area` varchar(500) NOT NULL,
+  `City` varchar(100) NOT NULL,
+  `State` varchar(100) NOT NULL,
+  `Country` varchar(100) NOT NULL,
+  `DOB` date NOT NULL,
+  `Gender` char(1) NOT NULL,
+  `Branch` varchar(100) NOT NULL,
+  `CGPA` float DEFAULT NULL,
+  `Semester` int unsigned DEFAULT NULL,
+  `Email` varchar(100) NOT NULL,
+  `Password` varchar(20) NOT NULL,
+  PRIMARY KEY (`Student ID`),
+  UNIQUE KEY `Student ID_UNIQUE` (`Student ID`),
+  KEY `College ID_Student_idx` (`College ID`),
+  CONSTRAINT `College ID_Student` FOREIGN KEY (`College ID`) REFERENCES `College` (`College ID`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 DROP TABLE IF EXISTS `Course`;
 CREATE TABLE `Course` (
   `Course ID` varchar(10) NOT NULL,
@@ -83,26 +106,43 @@ CREATE TABLE `Faculty` (
   CONSTRAINT `College ID_Faculty` FOREIGN KEY (`College ID`) REFERENCES `College` (`College ID`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-DROP TABLE IF EXISTS `Student`;
-CREATE TABLE `Student` (
-  `Student ID` int NOT NULL,
-  `First_Name` varchar(100) NOT NULL,
-  `Last_Name` varchar(100) NOT NULL,
+DROP TABLE IF EXISTS `Placement`;
+CREATE TABLE `Placement` (
   `College ID` varchar(10) NOT NULL,
-  `House Number` varchar(20) NOT NULL,
-  `Building,Street,Area` varchar(500) NOT NULL,
-  `City` varchar(100) NOT NULL,
-  `State` varchar(100) NOT NULL,
-  `Country` varchar(100) NOT NULL,
-  `DOB` date NOT NULL,
-  `Gender` char(1) NOT NULL,
-  `Branch` varchar(100) NOT NULL,
-  `CGPA` float DEFAULT NULL,
-  `Semester` int unsigned DEFAULT NULL,
+  `Year` int unsigned NOT NULL,
+  `Total Offers` int NOT NULL,
+  `Students Registered` int unsigned NOT NULL,
+  `Placement Percentage` float NOT NULL,
+  `Companies Visited` int unsigned NOT NULL,
+  `Average Package` float unsigned NOT NULL,
+  PRIMARY KEY (`College ID`,`Year`),
+  CONSTRAINT `College ID_Placement` FOREIGN KEY (`College ID`) REFERENCES `College` (`College ID`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+DROP TABLE IF EXISTS `Faculty Department`;
+CREATE TABLE `Faculty Department` (
+  `Name` varchar(100) NOT NULL,
+  `College ID` varchar(10) NOT NULL,
   `Email` varchar(100) NOT NULL,
-  `Password` varchar(20) NOT NULL,
-  PRIMARY KEY (`Student ID`),
-  UNIQUE KEY `Student ID_UNIQUE` (`Student ID`),
-  KEY `College ID_Student_idx` (`College ID`),
-  CONSTRAINT `College ID_Student` FOREIGN KEY (`College ID`) REFERENCES `College` (`College ID`) ON DELETE CASCADE ON UPDATE CASCADE
+  `HOD ID` varchar(10) NOT NULL,
+  PRIMARY KEY (`Name`,`College ID`),
+  KEY `College ID_Faculty Department_idx` (`College ID`),
+  KEY `HOD ID_Faculty Department_idx` (`HOD ID`),
+  CONSTRAINT `College ID_Faculty Department` FOREIGN KEY (`College ID`) REFERENCES `College` (`College ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `HOD ID_Faculty Department` FOREIGN KEY (`HOD ID`) REFERENCES `Faculty` (`Faculty ID`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+DROP TABLE IF EXISTS `Grade`;
+CREATE TABLE `Grade` (
+  `Student ID` varchar(10) NOT NULL,
+  `Course ID` varchar(10) NOT NULL,
+  `College ID` varchar(10) NOT NULL,
+  `Semester` int unsigned NOT NULL,
+  `Grade` float DEFAULT NULL,
+  PRIMARY KEY (`Student ID`,`Course ID`,`College ID`),
+  KEY `College ID_Grade_idx` (`College ID`),
+  KEY `Course ID_Grade_idx` (`Course ID`),
+  CONSTRAINT `College ID_Grade` FOREIGN KEY (`College ID`) REFERENCES `Course` (`Course ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `Course ID_Grade` FOREIGN KEY (`Course ID`) REFERENCES `Course` (`Course ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `Student ID_Grade` FOREIGN KEY (`Student ID`) REFERENCES `Student` (`Student ID`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
